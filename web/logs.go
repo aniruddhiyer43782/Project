dd677fd58a3fbf900fa88f28390162941576241a
Bhavik-1 <85212736+Bhavik-1@users.noreply.github.com>
2025-09-15 19:21:36 +0530

    Update logs.go

--- web/logs.go
+++ web/logs.go
@@ -7,15 +7,15 @@ import (
 	"os"
 )
 
-// MemoryLogs 内存中的日志
+
 type MemoryLogs struct {
-	MaxNum int      // 保存最大条数
-	Logs   []string // 日志
+	MaxNum int      
+	Logs   []string 
 }
 
 func (mlogs *MemoryLogs) Write(p []byte) (n int, err error) {
 	mlogs.Logs = append(mlogs.Logs, string(p))
-	// 处理日志数量
+	
 	if len(mlogs.Logs) > mlogs.MaxNum {
 		mlogs.Logs = mlogs.Logs[len(mlogs.Logs)-mlogs.MaxNum:]
 	}
@@ -24,7 +24,7 @@ func (mlogs *MemoryLogs) Write(p []byte) (n int, err error) {
 
 var mlogs = &MemoryLogs{MaxNum: 50}
 
-// 初始化日志
+
 func init() {
 	log.SetOutput(io.MultiWriter(mlogs, os.Stdout))
 	// log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
