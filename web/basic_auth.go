5bbaeb63f2689352ebf2acf0e805a31a53f2917d
Bhavik-1 <85212736+Bhavik-1@users.noreply.github.com>
2025-09-15 19:18:27 +0530

    Update basic_auth.go

--- web/basic_auth.go
+++ web/basic_auth.go
@@ -25,15 +25,15 @@ func BasicAuth(f ViewFunc) ViewFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		conf, _ := entity.GetConfigCache()
 
-		// 帐号或密码为空。跳过
+	
 		if conf.Username == "" && conf.Password == "" {
-			// 执行被装饰的函数
+			
 			f(w, r)
 			return
 		}
 
 		if ld.FailTimes >= 5 {
-			log.Printf("%s 登录失败超过5次! 并延时5分钟响应\n", r.RemoteAddr)
+			log.Printf("%s login failed more than 5 times! Response delayed by 5 minutes\n", r.RemoteAddr)
 			time.Sleep(5 * time.Minute)
 			if ld.FailTimes >= 5 {
 				ld.FailTimes = 0
@@ -42,14 +42,14 @@ func BasicAuth(f ViewFunc) ViewFunc {
 			return
 		}
 
-		// 认证帐号密码
+		
 		basicAuthPrefix := "Basic "
 
-		// 获取 request header
+		
 		auth := r.Header.Get("Authorization")
-		// 如果是 http basic auth
+		
 		if strings.HasPrefix(auth, basicAuthPrefix) {
-			// 解码认证信息
+			
 			payload, err := base64.StdEncoding.DecodeString(
 				auth[len(basicAuthPrefix):],
 			)
@@ -60,20 +60,19 @@ func BasicAuth(f ViewFunc) ViewFunc {
 					bytes.Equal(pair[0], []byte(conf.Username)) &&
 					bytes.Equal(pair[1], []byte(pwd)) {
 					ld.FailTimes = 0
-					// 执行被装饰的函数
+					
 					f(w, r)
 					return
 				}
 			}
 
 			ld.FailTimes = ld.FailTimes + 1
-			log.Printf("%s 登录失败!\n", r.RemoteAddr)
+			log.Printf("%s login failed!\n", r.RemoteAddr)
 		}
 
-		// 认证失败，提示 401 Unauthorized
-		// Restricted 可以改成其他的值
+		
 		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
-		// 401 状态码
+		
 		w.WriteHeader(http.StatusUnauthorized)
 		log.Printf("%s 请求登录!\n", r.RemoteAddr)
 	}
