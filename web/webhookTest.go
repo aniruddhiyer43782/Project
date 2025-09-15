f46234ded8bb4f58a9fcdd324e42a5f15cb209c6
Bhavik-1 <85212736+Bhavik-1@users.noreply.github.com>
2025-09-15 19:28:38 +0530

    Update webhookTest.go

--- web/webhookTest.go
+++ web/webhookTest.go
@@ -7,15 +7,15 @@ import (
 	"strings"
 )
 
-// WebhookTest 测试webhook
+
 func WebhookTest(writer http.ResponseWriter, request *http.Request) {
 	url := strings.TrimSpace(request.FormValue("URL"))
 	requestBody := strings.TrimSpace(request.FormValue("RequestBody"))
 	if url != "" {
 		wb := entity.Webhook{WebhookURL: url, WebhookRequestBody: requestBody}
-		wb.ExecWebhook(entity.BackupResult{ProjectName: "模拟测试", FileName: "2021-11-11_01_01.sql", FileSize: "100 MB", Result: "成功"})
+		wb.ExecWebhook(entity.BackupResult{ProjectName: "Simulation test", FileName: "2021-11-11_01_01.sql", FileSize: "100 MB", Result: "Success"})
 	} else {
-		log.Println("请输入Webhook的URL")
+		log.Println("Please enter the Webhook URL")
 	}
 
 }
